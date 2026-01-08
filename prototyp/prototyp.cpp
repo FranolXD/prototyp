2147ca9b54a326c7e13cad61613e2a3065123382
FranolXD <ook663036@gmail.com>
2026-01-08 18:04:10 +0100

    poprawienie błędu
    bo wtedy nie działało bo domyslnie robilem fora 2xtab i nie zmieniłem i na coś innego xD

--- prototyp/prototyp.cpp
+++ prototyp/prototyp.cpp
@@ -1,15 +1,15 @@
-﻿#include <algorithm> // dla max i min
+#include <iostream>// PODSTAWOWA BIBLIOTEKA 
+#include <algorithm> // dla max i min
 #include <limits>    // dla std::numeric_limits
 #include <string>// stringi
-#include <iostream>// PODSTAWOWA BIBLIOTEKA 
 #include <fstream>//odczyt pliku
 #include <cstdlib>//zeby CLS DZIALAL
 #include <vector>//wektory z tekstami
 #include <stdlib.h>//dla exit
-//#include <cctype>>>>> nie aktualne
+#include <cctype>
 #include <climits>//dla INT_MAX
 #include <windows.h>//do czekania ----dalej nie chce działać----
-#include <time.h>//dla exit
+#include <time.h>//dla sleep
 
 
 //wystarczylo napisac z duzej litery
@@ -39,7 +39,7 @@ std::string odpTekst(std::string pytanie) // funkcja pyt o tekst
 {
 	std::string odpowiedz;
 	std::cout << pytanie << "\n";
-	std::cin >> odpowiedz;  
+	std::cin >> odpowiedz;
 	return odpowiedz;
 }
 
@@ -57,7 +57,7 @@ int odpLiczba(std::string pytanie)// funkcja pyt o liczb cał
 {
 	int odpowiedz;
 	std::cout << pytanie << "\n";
-	std::cin >> odpowiedz;  
+	std::cin >> odpowiedz;
 	return odpowiedz;
 }
 int e; //zmienna zeby topka sie nie wykrzaczala
@@ -69,8 +69,7 @@ int l;//liczba ktora poda gracz
 int i;	//zmienna dla petli
 int pkt;//do sprawdzenia czy jest plik
 int a = 0;//żeby odczytywalo od samego poczatku xD
-//int f = 0; //zmienna do obliczenia pkt (tylko pytanie jaki stobić mnożnik pktów np całość to jakieś 1k potem -s razy cos innego co będzie zalezne od poziomu trudniści idk)
-	//nie aktualne
+int c = 0;
 
 std::string odptekst(std::string pyt) {
 	std::string odp;
@@ -80,7 +79,7 @@ std::string odptekst(std::string pyt) {
 }
 int odpliczb(std::string pyt) {
 	int odp;
-	std::cout << pyt << "\n";	
+	std::cout << pyt << "\n";
 	std::cin >> odp;
 	return odp;
 
@@ -90,14 +89,14 @@ int zgadnij(int x) {
 	int p = 1;//resetowanie prób przy ponownym graniu
 
 	do {
-		
+
 		while (!(l = odpliczb("\n					odgadnij liczbe"))) {
 			std::cin.clear(); // czyści flagę błędu strumienia
 			std::cin.ignore((std::numeric_limits<std::streamsize>::max)(), '\n'); // ignoruje niepoprawne dane wejściowe
 			std::cout << "to nie jest liczba :\n";
 		}
-		if (l < x) {	
-			std::cout <<"						"<< ZM(ZaMalo) << "					proba nr " << p << "\n\n";
+		if (l < x) {
+			std::cout << "						" << ZM(ZaMalo) << "					proba nr " << p << "\n\n";
 			p++;
 		}
 		else if (l > x) {
@@ -106,27 +105,26 @@ int zgadnij(int x) {
 		}
 	} while (l != x);
 	std::cout << "\n\n		Gratulacje Uzytkowniku, udalo ci sie odgadnac liczbe " << x << " na poziomie " << b << " w " << p << " podejsciach \n\n";
-	Sleep(1000);
 	f = odpTekst("\n						podaj swoj nick bambusie : \n");
 	if (b == "latwy") {
 		plik.open("top5GraczyLatwy.txt", std::ios::out | std::ios::app);//zeby dopisywao wyniki
 		plik << p << " razy  gracz " << f << " probowal odgadnac liczbe na poziomie " << b << "\n";
 		plik.close();
-		Sleep(3000);
+		Sleep(1000);
 		system("cls");
 	}
 	else if (b == "sredni") {
 		plik.open("top5GraczySredni.txt", std::ios::out | std::ios::app);//zeby dopisywao wyniki
 		plik << p << " razy  gracz " << f << " probowal odgadnac liczbe na poziomie " << b << "\n";
 		plik.close();
-		Sleep(3000);
+		Sleep(1000);
 		system("cls");
 	}
 	else if (b == "trudny") {
 		plik.open("top5GraczyTrudny.txt", std::ios::out | std::ios::app);//zeby dopisywao wyniki
 		plik << p << " razy  gracz " << f << " probowal odgadnac liczbe na poziomie " << b << "\n";
 		plik.close();
-		Sleep(3000);
+		Sleep(1000);
 		system("cls");
 	}
 	else if (b == "custom") {
@@ -134,7 +132,7 @@ int zgadnij(int x) {
 		plik.open("top5SuperGraczyWZgadnijLiczbeXD.txt", std::ios::out | std::ios::app);//zeby dopisywao wyniki
 		plik << p << " razy  gracz " << f << " probowal odgadnac liczbe na poziomie " << b << "\n";
 		plik.close();
-		Sleep(3000);
+		Sleep(1000);
 		system("cls");
 	}
 	return x;
@@ -145,7 +143,7 @@ int main()
 	for (i = 1; i <= 1;) { //tu będzie pętla 
 		std::cout << "\n\n					Ponizej sa dostepne opjce\n\n			[graj]			[top 5]			[wyjscie]\n\n";
 		std::string wybor = odpTekst("\n			  		  Wpisz swoj wybor: "); //co dalej?
-		system("cls");
+		
 		if (wybor == "graj" || wybor == "play" || wybor == "p" || wybor == "g") {
 			/*char zak = odpchar("\n	Psst \nMoze jakis zakladzik :3 ?\n			[ t / n ]");//bo switch daiala tylko z intem i charem
 			switch (zak)
@@ -183,14 +181,15 @@ int main()
 				b = "custom";
 				if (z > 1 && z < INT_MAX) {
 					std::cout << "					Dobra to lecimy";
-					Sleep(2000);
+					Sleep(1000);
 					x = rand() % z + 1;
 				}
 				else
-				{ 
-				z = INT_MAX; // jeśli bedzie minus albo wieksz od max
-					std::cout << "						zła odpowiedź \n					<{ WŁĄCZAM TRYB ULTIMATE }>\n";
+				{
+					z = INT_MAX; // jeśli bedzie minus albo wieksz od max
+					std::cout << "						zla odpowiedz \n					<{ WLACZAM TRYB ULTIMATE }>\n";
 					x = rand() % z + 1;
+					Sleep(1000);
 				}
 			}
 			system("cls");
@@ -209,7 +208,8 @@ int main()
 					plik.open("top5GraczyLatwy.txt", std::ios::in); //to bedzie otwieralo plik
 					if (plik.good() == false) {// a to bedzie sprawdzalo czy plik istnieje a jakl nie to notka
 						std::cout << "\n\n			Najpierw wykrec jakis dobry wynik mlody padawanie :P\n\n";
-
+						Sleep(1000);
+						q++;
 					}
 					else {
 						Wyniki.clear();//czyszcenie wektora zeby wszystko bylo ladnie pieknie
@@ -218,15 +218,16 @@ int main()
 						}
 						std::sort(Wyniki.begin(), Wyniki.end());//sortowanie wektora
 						if (Wyniki.size() < 6) {
-						e = Wyniki.size();
+							e = Wyniki.size();
 						}
 						else {
 							e = 6;
 						}
-						for (i = 0; i < e; i++) {
-							std::cout << "\n" << Wyniki[i];
+						for (c = 0; c < e; c++) {
+							std::cout << "\n" << Wyniki[c];
 						}
 						plik.close();
+						Sleep(1000);
 						q++;
 					}
 				}
@@ -234,7 +235,8 @@ int main()
 					plik.open("top5GraczySredni.txt", std::ios::in); //to bedzie otwieralo plik
 					if (plik.good() == false) {// a to bedzie sprawdzalo czy plik istnieje a jakl nie to notka
 						std::cout << "\n\n			Najpierw wykrec jakis dobry wynik mlody padawanie ;)\n\n";
-
+						Sleep(1000);
+						q++;
 					}
 					else {
 						Wyniki.clear();//czyszcenie wektora zeby wszystko bylo ladnie pieknie
@@ -248,10 +250,11 @@ int main()
 						else {
 							e = 6;
 						}
-						for (i = 0; i < e; i++) {
-							std::cout << "\n" << Wyniki[i];
+						for (c = 0; c < e; c++) {
+							std::cout << "\n" << Wyniki[c];
 						}
 						plik.close();
+						Sleep(1000);
 						q++;
 					}
 				}
@@ -259,7 +262,8 @@ int main()
 					plik.open("top5GraczyTrudny.txt", std::ios::in); //to bedzie otwieralo plik
 					if (plik.good() == false) {// a to bedzie sprawdzalo czy plik istnieje a jakl nie to notka
 						std::cout << "\n\n			Najpierw wykrec jakis dobry wynik mlody padawanie ;)\n\n";
-
+						Sleep(1000);
+						q++;
 					}
 					else {
 						Wyniki.clear();//czyszcenie wektora zeby wszystko bylo ladnie pieknie
@@ -273,10 +277,11 @@ int main()
 						else {
 							e = 6;
 						}
-						for (i = 0; i < e; i++) {
-							std::cout << "\n" << Wyniki[i];
+						for (c = 0; c < e; c++) {
+							std::cout << "\n" << Wyniki[c];
 						}
 						plik.close();
+						Sleep(1000);
 						q++;
 					}
 				}
@@ -284,7 +289,8 @@ int main()
 					plik.open("top5SuperGraczyWZgadnijLiczbeXD.txt", std::ios::in); //to bedzie otwieralo plik
 					if (plik.good() == false) {// a to bedzie sprawdzalo czy plik istnieje a jakl nie to notka
 						std::cout << "			Najpierw wykrec jakis dobry wynik mlody padawanie ;)\n\n";
-
+						Sleep(1000);
+						q++;
 					}
 					else {
 						Wyniki.clear();//czyszcenie wektora zeby wszystko bylo ladnie pieknie
@@ -298,24 +304,25 @@ int main()
 						else {
 							e = 6;
 						}
-						for (i = 0; i < e; i++) {
+						for (c = 0; c < e; c++) {
 							std::cout << "\n" << Wyniki[i];
 						}
 						plik.close();
+						Sleep(1000);
 						q++;
 					}
 				}
 				else {
 					std::cout << "\n				nie ma takiego poziomu mordeczko	'o' \n\n";
+					
 				}
-				Sleep(5000); 
 				a = 0;//resetowanie zeby przy ponownym wyswietleniu topki dzialalo
 				system("cls");
+
 			}
 		}
 		else if (wybor == "wyjscie" || wybor == "w" || wybor == "exit" || wybor == "e") {
-			std::cout << "\n						No to Siema Byczq\n\n";
-			Sleep(2000);
+			std::cout << "\n						No to Siema Byczq \n \a";
 			exit(0);//zamykanie programu
 		}
 		else if (wybor == "napoj" || wybor == "n") {
@@ -323,16 +330,15 @@ int main()
 		}
 		else if (wybor == "misiu" || wybor == "m") {
 			std::cout << "\n		Misiu cie pozdrawia\n		Takze czuj sie wyjatkowy";
-			Sleep(3000);
 		}
 		else if (wybor == "2137") {
 			std::cout << "oj nie ladnie\a  \a  \a  \a  \a  \a  \a  \a  \a  \a  \a  \a\n";
-			for ( e = 0; e < 10; e++)
+			for (e = 0; e < 10; e++)
 			{
 				Sleep(500);
 				std::cout << "\a papiez cie obserwuje \a\n";
-		}
-		//R po to by dalo sie nie bawic z zwyklym stringiem bo on papieza nie lubi
+			}
+			//R po to by dalo sie nie bawic z zwyklym stringiem bo on papieza nie lubi
 			std::cout << R"(
 ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⡠⠤⠲⡆⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
 ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⡴⢛⣁⣤⣤⣤⡤⠤⠤⠄⠤⢄⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
@@ -356,21 +362,17 @@ int main()
 ⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣧⣄⡈⠙⣿⣿⣿⣿⡆⠀⠙⣿⣿⡆⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
 ⠉⠉⠉⠉⠉⠉⠉⠉⠉⠉⠉⠉⠉⠉⠉⠉⠉⠉⠉⠉⠉⠉⠉⠉⠁⠀⠀⠉⠉⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
 
-				ja nie wieze papiez polak w komputerze  <3  <3 <3)";// do poprawki bo sie drukowac nie chce"""""""""""""""""""""""""""""""""""""""""'
-			Sleep(3000);
-}
+				ja nie wieze papiez polak w komputerze  <3  <3 <3)";// do poprawki bo sie drukowac nie chce""""""""""""""""""""""""""""""""""""
+		}
 
-	
+
 		else {
 			std::cout << "\n				Czlowieku zastanow sie, badz soba, LUDZIU!\n";
-			Sleep(3000);
 			system("cls");
 		}
 	}
 	std::cout << "\n				Dziekuje za uwage\n";
 	std::cout << "\n		I prosze misia zostawic w spokoju\n";
 	std::cout << "\n			Bo misiu idzie sie teraz napic\n";
-	Sleep(2000);
 	exit(0);//zamykanie programu
-
 }
